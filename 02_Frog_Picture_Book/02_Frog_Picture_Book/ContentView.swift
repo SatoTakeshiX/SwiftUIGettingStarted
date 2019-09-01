e4a0cf14b856d2c9615099bd1c05d47a38ba3d57
satoutakeshi <sato.takeshi1987@gmail.com>
2019-09-01 10:28:35 +0900

    create frog picture book

---
+++ 02_Frog_Picture_Book/02_Frog_Picture_Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02_Frog_Picture_Book
+//
+//  Created by satoutakeshi on 2019/09/01.
+//  Copyright © 2019 satoutakeshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 02_Frog_Picture_Book/02_Frog_Picture_BookTests/_2_Frog_Picture_BookTests.swift
@@ -0,0 +1,34 @@
+//
+//  _2_Frog_Picture_BookTests.swift
+//  02_Frog_Picture_BookTests
+//
+//  Created by satoutakeshi on 2019/09/01.
+//  Copyright © 2019 satoutakeshi. All rights reserved.
+//
+
+import XCTest
+@testable import _2_Frog_Picture_Book
+
+class _2_Frog_Picture_BookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
